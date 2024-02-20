d0def3cc363194ca0919cabab2edc88b6794767a
nighlabs <8534496+nighlabs@users.noreply.github.com>
2024-02-19 22:24:57 -0500

    Replace tf wait on terraform config

--- terraform/kubedoo-lonelynode/main.tf
+++ terraform/kubedoo-lonelynode/main.tf
@@ -45,5 +45,11 @@ data "talos_cluster_kubeconfig" "this" {
   depends_on           = [talos_machine_bootstrap.this]
   client_configuration = talos_machine_secrets.this.client_configuration
   node                 = var.cluster_vip
-  wait                 = true
+}
+
+data "talos_cluster_health" "this" {
+  depends_on            = [data.talos_cluster_kubeconfig.this]
+  client_configuration  = talos_machine_secrets.this.client_configuration
+  control_plane_nodes   = [for k, v in var.node_data.controlplanes : k]
+  endpoints             = [for k, v in var.node_data.controlplanes : k]
 }
